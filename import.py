4f225ace17e36a4aeaedac0635eed5cc4e6fc783
Ziemowit Pałka <ziemowit.palka@cdrl.local>
2024-10-08 14:56:04 +0200

    req.txt file added

--- import.py
+++ import.py
@@ -81,8 +81,9 @@ def escape_identifier(identifier):
 
 
 # Database connection details
-server = os.getenv('server')
-database = os.getenv('database')
+# CHANGE THIS WITH NEXT PUSH
+server = 'cdrl-bidata02'
+database = 'systel'
 
 connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
 from datetime import datetime
